71c8c56bbb3f7c0763841a95a517605355dc2c31
Sam Davarnia <>
2017-12-30 15:00:35 -0800

    specs

--- spec/broker_factory_spec.rb
+++ spec/broker_factory_spec.rb
@@ -1,26 +1,33 @@
 RSpec.describe Eventsourcer::BrokerFactory do
   describe ".build" do
+    context "when broker is not configured" do
+      it "raises BrokerNotFound" do
+        expect{ described_class.build}.to raise_error(Eventsourcer::Errors::BrokerNotConfiguredError)
+      end
+    end
+
     context "when broker is :kafka" do
       it "returns KafkaBroker" do
         Eventsourcer.configure { |config| config.broker = :kafka }
-        allow(described_class).to receive(:borker).and_return(:kafka)
+        allow(described_class).to receive(:fetch_broker_klass).with(:kafka).and_return(Eventsourcer::Brokers::KafkaBroker)
         expect(described_class.build).to eq(Eventsourcer::Brokers::KafkaBroker)
       end
     end
   end
 
-  describe ".broker" do
+  describe ".fetch_broker_klass" do 
     context "when broker is configured" do
       it "returns configured broker" do
         Eventsourcer.configure { |config| config.broker = :kafka }
-        expect(described_class.broker).to eq(:kafka)
+        expect(described_class.fetch_broker_klass(:kafka)).to eq(Eventsourcer::Brokers::KafkaBroker)
+
       end
     end
 
-    context "when broker is not configured" do
-      it "raises BrokerNotConfiguredError" do
-        Eventsourcer.configure { |config| config.broker = nil }
-        expect{ described_class.broker}.to raise_error(Eventsourcer::Errors::BrokerNotConfiguredError)
+    context "when broker is not found" do
+      it "raises BrokerNotFound" do
+        Eventsourcer.configure { |config| config.broker = "" }
+        expect{ described_class.fetch_broker_klass(nil)}.to raise_error(Eventsourcer::Errors::BrokerNotFound)
       end
     end
   end
